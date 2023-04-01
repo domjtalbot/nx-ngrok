a6c2aeb8b8141e519ec74209aef988c43c82d9f8
Dom <3301714+domjtalbot@users.noreply.github.com>
2023-04-01 20:29:41 +0100

    refactor(nx-ngrox): remove build executor

--- packages/nx-ngrok/src/executors/build/build.ts
+++
@@ -1,8 +0,0 @@
-import { BuildExecutorSchema } from './schema';
-
-export default async function buildExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build', options);
-  return {
-    success: true,
-  };
-}
