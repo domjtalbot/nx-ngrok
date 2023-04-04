6f2790c4288e530f6d875d6cd964a818a7c6b719
Dom <3301714+domjtalbot@users.noreply.github.com>
2023-04-04 13:18:05 +0100

    feat(nx-ngrok): add tunnel target generator

---
+++ packages/nx-ngrok/src/generators/tunnel/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { TunnelGeneratorSchema } from './schema';
+
+describe('tunnel generator', () => {
+  let appTree: Tree;
+  const options: TunnelGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ packages/nx-ngrok/src/generators/tunnel/schema.d.ts
@@ -0,0 +1,5 @@
+export interface TunnelGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
